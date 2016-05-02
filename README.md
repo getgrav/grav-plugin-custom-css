e4db01c7802d15a1e4e65ec8c5ee8ce2e4ed350c
Flavio Copes <copesc@gmail.com>
2016-05-02 18:44:18 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Custom CSS Plugin
+
+The **Custom CSS** Plugin for the [Grav CMS](http://github.com/getgrav/grav) allows to add additional CSS to your site, without editing the theme or having to create a plugin for the simple task of adding some custom lines of sylesheets.
+
+## Description
+
+Adds some custom CSS to your Grav site.
+
+Open the plugin preferences in the Admin panel, and add custom CSS directly in the textarea, which will be added as inline CSS, or enter one or more relative paths to load. (e.g. /user/my_css/custom.css)
---
+++ custom-css.php
@@ -0,0 +1,46 @@
+<?php
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+use RocketTheme\Toolbox\Event\Event;
+
+/**
+ * Class CustomCSSPlugin
+ * @package Grav\Plugin
+ */
+class CustomCSSPlugin extends Plugin
+{
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0]
+        ];
+    }
+
+    /**
+     * Initialize the plugin
+     */
+    public function onPluginsInitialized()
+    {
+        // Don't proceed if we are in the admin plugin
+        if ($this->isAdmin()) {
+            return;
+        }
+
+        $this->enable([
+            'onAssetsInitialized' => ['onAssetsInitialized', 0]
+        ]);
+    }
+
+    public function onAssetsInitialized()
+    {
+        $this->grav['assets']->addInlineCss($this->config->get('plugins.custom-css.css_inline'));
+
+        foreach($this->config->get('plugins.custom-css.css_files') as $file) {
+            $this->grav['assets']->addCss($file['path']);    
+        }    
+    }
+}
